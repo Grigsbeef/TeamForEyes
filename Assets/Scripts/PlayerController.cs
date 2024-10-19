237511266a8526834068f67d62cd8e8df7451e84
Grigsbeef <cgrigsby2001@gmail.com>
2024-10-18 18:37:44 -0700

    MovementJump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,25 +6,38 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb2d;
     public float speed = 5f;
+    public float jumpHeight = 3f;
+    public float jumpForward = 2f;
     float horizontal;
     float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        //rb2d = GetComponent<Rigidbody2D>();   
+        rb2d = GetComponent<Rigidbody2D>();   
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+       vertical = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb2d.AddForce(transform.up * jumpHeight);
+            rb2d.AddForce(transform.forward * jumpHeight);
+        }
     }
 
     private void FixedUpdate()
     {
         rb2d.velocity = new Vector2(horizontal * speed, vertical * speed);
+        
     }
 
+    void Jump()
+    {
+        
+    }
 
 }
