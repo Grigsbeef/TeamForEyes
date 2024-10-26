5ec5499a46fa0ef59c76c27cb5703346e38f09b0
Grigsbeef <cgrigsby2001@gmail.com>
2024-10-25 18:22:02 -0700

    10/25 Changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,38 +6,48 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb2d;
     public float speed = 5f;
-    public float jumpHeight = 3f;
+    public float jumpHeight = 100f;
     public float jumpForward = 2f;
     float horizontal;
     float vertical;
+    bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();   
-        
+        isGrounded = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-       vertical = Input.GetAxisRaw("Vertical");
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb2d.AddForce(transform.up * jumpHeight);
-            rb2d.AddForce(transform.forward * jumpHeight);
-        }
+       horizontal = Input.GetAxisRaw("Horizontal");
+       //vertical = Input.GetAxisRaw("Vertical");
+        
     }
 
     private void FixedUpdate()
     {
         rb2d.velocity = new Vector2(horizontal * speed, vertical * speed);
-        
+        if (Input.GetKey(KeyCode.W) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Grounded"))
+        {
+            isGrounded = true;
+        }
     }
 
     void Jump()
     {
-        
+        rb2d.AddForce(transform.up * jumpHeight,ForceMode2D.Impulse);
+        //rb2d.AddForce(transform.forward * jumpHeight, ForceMode2D.Impulse);
+        isGrounded = false;
     }
 
 }
